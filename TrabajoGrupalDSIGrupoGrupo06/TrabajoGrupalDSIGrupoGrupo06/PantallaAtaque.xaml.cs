f10c86fc53c64716f09fbc69262e97913a837a82
damart24 <82331644+damart24@users.noreply.github.com>
2022-04-18 18:49:24 +0200

    Click para atacar

--- TrabajoGrupalDSIGrupoGrupo06/TrabajoGrupalDSIGrupoGrupo06/PantallaAtaque.xaml.cs
+++ TrabajoGrupalDSIGrupoGrupo06/TrabajoGrupalDSIGrupoGrupo06/PantallaAtaque.xaml.cs
@@ -32,6 +32,49 @@ namespace TrabajoGrupalDSIGrupoGrupo06
             Frame.Navigate(typeof(PantallaJuego));
         }
 
-        
+        private void Edificio_Click(object sender, RoutedEventArgs e)
+        {
+            NombreAtaque1.Visibility = Visibility.Collapsed;
+            IconoLucha1.Visibility = Visibility.Collapsed;
+            NombreAtaque2.Visibility = Visibility.Collapsed;
+            IconoLucha2.Visibility = Visibility.Collapsed;
+            NombreAtaque3.Visibility = Visibility.Collapsed;
+            IconoLucha3.Visibility = Visibility.Collapsed;
+            NombreAtaque4.Visibility = Visibility.Collapsed;
+            IconoLucha4.Visibility = Visibility.Collapsed;
+            NombreAtaque5.Visibility = Visibility.Collapsed;
+            IconoLucha5.Visibility = Visibility.Collapsed;
+            NombreAtaque6.Visibility = Visibility.Collapsed;
+            IconoLucha6.Visibility = Visibility.Collapsed;
+            Button button = (Button)sender;
+            string buttonName = button.Name;
+            switch (buttonName)
+            {
+                case "Edificio_1":
+                    NombreAtaque1.Visibility = Visibility.Visible;
+                    IconoLucha1.Visibility = Visibility.Visible;
+                    break;
+                case "Edificio_2":
+                    NombreAtaque2.Visibility = Visibility.Visible;
+                    IconoLucha2.Visibility = Visibility.Visible;
+                    break;
+                case "Edificio_3":
+                    NombreAtaque3.Visibility = Visibility.Visible;
+                    IconoLucha3.Visibility = Visibility.Visible;
+                    break;
+                case "Edificio_4":
+                    NombreAtaque4.Visibility = Visibility.Visible;
+                    IconoLucha4.Visibility = Visibility.Visible;
+                    break;
+                case "Edificio_5":
+                    NombreAtaque5.Visibility = Visibility.Visible;
+                    IconoLucha5.Visibility = Visibility.Visible;
+                    break;
+                case "Edificio_6":
+                    NombreAtaque6.Visibility = Visibility.Visible;
+                    IconoLucha6.Visibility = Visibility.Visible;
+                    break;
+            }
+        }
     }
 }
