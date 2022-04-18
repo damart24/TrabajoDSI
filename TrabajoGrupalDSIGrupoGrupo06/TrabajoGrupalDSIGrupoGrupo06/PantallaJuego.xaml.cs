e1f11cfe5d3faccac282d6898fb0709097ce98d9
damart24 <82331644+damart24@users.noreply.github.com>
2022-04-18 16:34:47 +0200

    Se suben de nivel campamentos y minas

--- TrabajoGrupalDSIGrupoGrupo06/TrabajoGrupalDSIGrupoGrupo06/PantallaJuego.xaml.cs
+++ TrabajoGrupalDSIGrupoGrupo06/TrabajoGrupalDSIGrupoGrupo06/PantallaJuego.xaml.cs
@@ -11,6 +11,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=234238
@@ -22,8 +23,12 @@ namespace TrabajoGrupalDSIGrupoGrupo06
     /// </summary>
     public sealed partial class PantallaJuego : Page
     {
+        string mina1Name_, mina2Name_, mina3Name_;
+        string campamento1Name_, campamento2Name_, campamento3Name_;
         public PantallaJuego()
         {
+            campamento1Name_ = campamento2Name_ = campamento3Name_ = "ms-appx:///Assets/cuartel_nivel1.png";
+            mina1Name_ = mina2Name_ = mina3Name_ = "ms-appx:///Assets/mina_nivel1.png";
             this.InitializeComponent();
         }
 
@@ -63,6 +68,97 @@ namespace TrabajoGrupalDSIGrupoGrupo06
         private void Pantalla_AtaqueNavegacion(object sender, RoutedEventArgs e)
         {
             Frame.Navigate(typeof(PantallaAtaque));
+        }
+
+        private void Mina_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            string buttonName = button.Name;
+            switch (buttonName)
+            {
+                case "BotonMina1":
+                    if (mina1Name_ == "ms-appx:///Assets/mina_nivel1.png")
+                    {
+                        mina1Name_ = "ms-appx:///Assets/mina_nivel2.png";
+                        Mina1.Source = new BitmapImage(new Uri("ms-appx:///Assets/mina_nivel2.png"));
+                    }
+                    else if (mina1Name_ == "ms-appx:///Assets/mina_nivel2.png")
+                    {
+                        mina1Name_ = "ms-appx:///Assets/mina_nivel3.png";
+                        Mina1.Source = new BitmapImage(new Uri("ms-appx:///Assets/mina_nivel3.png"));
+                    }
+                    break;
+                case "BotonMina2":
+                    if (mina2Name_ == "ms-appx:///Assets/mina_nivel1.png")
+                    {
+                        mina2Name_ = "ms-appx:///Assets/mina_nivel2.png";
+                        Mina2.Source = new BitmapImage(new Uri("ms-appx:///Assets/mina_nivel2.png"));
+                    }
+                    else if (mina2Name_ == "ms-appx:///Assets/mina_nivel2.png")
+                    {
+                        mina2Name_ = "ms-appx:///Assets/mina_nivel3.png";
+                        Mina2.Source = new BitmapImage(new Uri("ms-appx:///Assets/mina_nivel3.png"));
+                    }
+                    break;
+                case "BotonMina3":
+                    if (mina3Name_ == "ms-appx:///Assets/mina_nivel1.png")
+                    {
+                        mina3Name_ = "ms-appx:///Assets/mina_nivel2.png";
+                        Mina3.Source = new BitmapImage(new Uri("ms-appx:///Assets/mina_nivel2.png"));
+                    }
+                    else if (mina3Name_ == "ms-appx:///Assets/mina_nivel2.png")
+                    {
+                        mina3Name_ = "ms-appx:///Assets/mina_nivel3.png";
+                        Mina3.Source = new BitmapImage(new Uri("ms-appx:///Assets/mina_nivel3.png"));
+                    }
+                    break;
+            }
+        }
+
+        private void Campamento_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            string buttonName = button.Name;
+            switch (buttonName)
+            {
+                case "BotonCampamento1":
+                    if (campamento1Name_ == "ms-appx:///Assets/cuartel_nivel1.png")
+                    {
+                        campamento1Name_ = "ms-appx:///Assets/cuartel_nivel2.png";
+                        Campamento1.Source = new BitmapImage(new Uri("ms-appx:///Assets/cuartel_nivel2.png"));
+                    }
+                    else if (campamento1Name_ == "ms-appx:///Assets/cuartel_nivel2.png")
+                    {
+                        campamento1Name_ = "ms-appx:///Assets/cuartel_nivel3.png";
+                        Campamento1.Source = new BitmapImage(new Uri("ms-appx:///Assets/cuartel_nivel3.png"));
+                    }
+                    break;
+                case "BotonCampamento2":
+                    if (campamento2Name_ == "ms-appx:///Assets/cuartel_nivel1.png")
+                    {
+                        campamento2Name_ = "ms-appx:///Assets/cuartel_nivel2.png";
+                        Campamento2.Source = new BitmapImage(new Uri("ms-appx:///Assets/cuartel_nivel2.png"));
+                    }
+                    else if (campamento2Name_ == "ms-appx:///Assets/cuartel_nivel2.png")
+                    {
+                        campamento2Name_ = "ms-appx:///Assets/cuartel_nivel3.png";
+                        Campamento2.Source = new BitmapImage(new Uri("ms-appx:///Assets/cuartel_nivel3.png"));
+                    }
+                    break;
+                case "BotonCampamento3":
+                    if (campamento3Name_ == "ms-appx:///Assets/cuartel_nivel1.png")
+                    {
+                        campamento3Name_ = "ms-appx:///Assets/cuartel_nivel2.png";
+                        Campamento3.Source = new BitmapImage(new Uri("ms-appx:///Assets/cuartel_nivel2.png"));
+                    }
+                    else if (campamento3Name_ == "ms-appx:///Assets/cuartel_nivel2.png")
+                    {
+                        campamento3Name_ = "ms-appx:///Assets/cuartel_nivel3.png";
+                        Campamento3.Source = new BitmapImage(new Uri("ms-appx:///Assets/cuartel_nivel3.png"));
+                    }
+                    break;
+            }
+
         }
     }
 }
